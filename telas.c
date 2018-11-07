8ec40f4e2a28010b877934884f2b4052224cbf7d
Felipe Rodrigues do Nascimento <felipekjr@A307-34L.imd.local>
2018-11-07 10:33:39 -0300

    Definindo estrutura

--- telas.c
+++ telas.c
@@ -3,9 +3,22 @@
 #include <string.h>
 #include "tabela.h"
 
+int definirAtributos();
+
+typedef struct tabela{
+    int id;
+    char *nome;
+}Tabela;
+
+typedef struct atributo{
+    char *nome;
+    int tipo;
+    int tamanho;
+}Atributo;
+
+
 void v_criarTabela(){    
-    	char * nome = NULL; //Nome da tabela-arquivo.txt
-    	
+    char * nome = NULL; //Nome da tabela-arquivo.txt    
 	//nomeando o arquivo de texto
 	printf("Digite o nome da sua tabela: ");
     	getchar();
@@ -15,8 +28,29 @@ void v_criarTabela(){
     if(criarTabela(nome)<0){
         printf("Falha ao criar arquivo!\n");
     }else{
-        printf("Arquivo criado com sucesso!\n");
+        if(definirAtributos()){
+
+        }
     }
+}
+
+int definirAtributos(){
+    int count = 1;
+    Atributo *ptr_att = (Atributo*) calloc(1,sizeof(Atributo));   
+
+    //tela de tipos
+    int ctr;
+    do{
+        printf("Digite o tipo da coluna: \n\n1-CHAR\n2-INT\n3-FLOAT\n4-DOUBLE\n5-STRING\n0-FIM\n");
+        scanf("%d", &ctr);
+        switch(ctr){
+            
+            case 1:
+
+        }
+    }while(ctr != 0)
+   
+
 }
 
 void v_listarTabelas(){}
