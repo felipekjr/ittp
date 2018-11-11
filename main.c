01e191021597c1d9440304623730c305eb57514e
Yago Marques <44405378+yagobmarques@users.noreply.github.com>
2018-11-11 17:44:09 -0200

    Organizando as funções com seus devidos checkpoint

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
 	int op = 0;
 	do{
 
-	printf("Escolha as funcionalidades:\n   1-Criar uma tabela\n   2-Listar todas as tabelas\n   3-Criar uma nova linha na tabela\n   4-Listar todos os dados de uma tabela\n   5-Pesquisar um valor em uma tabela\n   6-Apagar valor de uma tabela\n   7-Apagar uma tabela\n");
+	printf("Escolha as funcionalidades:\n   0-Sair\n   1-Criar uma tabela\n   2-Listar todas as tabelas\n   3-Criar uma nova linha na tabela\n   4-Listar todos os dados de uma tabela\n   5-Pesquisar um valor em uma tabela\n   6-Apagar valor de uma tabela\n   7-Apagar uma tabela\n");
 	scanf("%d", &op);
 	switch (op)
 		{
